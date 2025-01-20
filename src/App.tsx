c4bbb6b250f8f5b51affc9a13abb93260a258e94
persistsolution <persistsolution1@gmail.com>
2025-01-21 01:09:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h1>Staging</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Staginingss</h1>
+      <h1>Vite + React + TypeScript  </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
