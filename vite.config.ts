41e30fb54ae39625beae706b5d04c485dff85ec9
persistsolution <persistsolution1@gmail.com>
2025-01-20 17:23:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 5173,      // Adjust for each branch
+  },
+});
