fa2fb131b9c979138e2c5e9f8e7bb681c28d03ca
persistsolution <persistsolution1@gmail.com>
2025-01-21 00:49:28 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Production </h1>
+      <h1>Vite + React + TypeScript </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          <h1>Production</h1>
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
