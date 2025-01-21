c50e759a7ce5ea6d8cbe5ec266ad0743fbc61e25
persistsolution <persistsolution1@gmail.com>
2025-01-21 18:15:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Kwickbill Testing</h1>
+        <h1>Testing</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
