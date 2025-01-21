02ebeec1d998bfe07796c23166c0992bcd2804fc
persistsolution <persistsolution1@gmail.com>
2025-01-21 15:35:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 // App.tsx
 import React from 'react';
-import viteLogo from '/vite.svg';  // Referencing the file from the root of the project
 
 function App() {
   return (
     <div>
       <h1>Vite + React</h1>
-      <img src={viteLogo} alt="Vite logo" />
+      {/* Removed the reference to viteLogo and the image */}
     </div>
   );
 }
