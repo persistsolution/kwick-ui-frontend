4ea956b11b235c739d15e759aa569a652967b0ab
persistsolution <persistsolution1@gmail.com>
2025-01-21 16:09:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Docker</h1>
       {/* Removed the reference to viteLogo and the image */}
     </div>
   );
