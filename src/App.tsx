bef011a7297b3f52f800e91855df51a49115ea8d
persistsolution <persistsolution1@gmail.com>
2025-01-21 16:35:01 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,9 @@ function App() {
     <>
       <div>
         <h1>Docker Test</h1>
-       
+       <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
