04347ddc624022c84cf812946c888694d97564b7
nealgiradkar <nileshgiradkar1@gmail.com>
2025-01-22 16:18:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Production New</h1>
+        <h1>Production</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
