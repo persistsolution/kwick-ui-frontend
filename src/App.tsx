067bf6aa97748d8f78b0bf9b98a08c7c8e0fa41b
Persist Solutions <persistsolution1@gmail.com>
2025-01-22 16:46:51 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + TypeScript + Bun Runtime</h1>
+      <h1>Vite + React + TypeScript + Bun Runtime dd</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
